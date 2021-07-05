1612db55b4d7c81a95ad79df5ebf746ae942571d
Rafael Fernandes <rafaelfernandesmarcondes@gmail.com>
2021-07-05 15:00:09 +0000

    +staticfiles-route

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,6 +29,14 @@ BASE_DIR = os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..")
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
